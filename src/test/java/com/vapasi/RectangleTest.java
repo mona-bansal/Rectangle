f92488a69a825f027883dd2ed7a0faa55462de58
mona1.bansal <mona1.bansal@WORKSPACES.THOUGHTWORKS.COM>
2020-11-24 12:15:57 +0530

    [Mona/Indira] remove redundant testcases and method name change

--- src/test/java/com/vapasi/RectangleTest.java
+++ src/test/java/com/vapasi/RectangleTest.java
@@ -3,27 +3,13 @@ package com.vapasi;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class RectangleTest {
 
-
     @Test
-   public void shouldGiveAreaOneForLengthOneBreadthOne() {
-        Rectangle rectangle = new Rectangle(1, 1);
-        double area = rectangle.getArea();
-        assertEquals (1, area);
-    }
-
-    @Test
-    public void shouldGiveAreaOneForLengthTwoBreadthThree() {
-        Rectangle rectangle = new Rectangle(2, 3);
-        double area = rectangle.getArea();
-        assertEquals (6, area);
-    }
-
-    @Test
-    public void shouldGiveAreaLMultipledByBForAnyRectange() {
+    public void shouldGiveAreaForAnyCoOrdinates() {
         double len = 12.3;
         double br = 14.6;
         Rectangle rectangle = new Rectangle(len, br);
@@ -31,4 +17,5 @@ public class RectangleTest {
         double expected = len * br;
         assertEquals (expected, area);
     }
+
 }

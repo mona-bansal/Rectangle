55cb695d9273f85658a0125f46c762072df513e7
raghavan.indira <raghavan.indira@WORKSPACES.THOUGHTWORKS.COM>
2020-11-24 12:17:43 +0530

    [Indira/Mona] Removed dead code

--- src/test/java/com/vapasi/RectangleTest.java
+++ src/test/java/com/vapasi/RectangleTest.java
@@ -3,7 +3,6 @@ package com.vapasi;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class RectangleTest {
